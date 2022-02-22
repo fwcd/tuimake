18cf8c9a051293aee7a642b966061b8e4e4f7eaf
fwcd <fwcdmail@gmail.com>
2022-02-22 16:40:56 +0100

    Add exit handlers

--- src/Tuimake/App.hs
+++ src/Tuimake/App.hs
@@ -3,19 +3,26 @@ module Tuimake.App
   ) where
 
 import Control.Monad (void)
+import Control.Monad.IO.Class (liftIO)
 import qualified Graphics.Vty as V
 import qualified Brick.AttrMap as BA
 import qualified Brick.Main as BM
 import qualified Brick.Types as BT
+import qualified Brick.Widgets.Core as BW
 
 initialState :: ()
 initialState = ()
 
 appEvent :: () -> BT.BrickEvent () e -> BT.EventM () (BT.Next ())
+appEvent st (BT.VtyEvent e) =
+  case e of
+    V.EvKey (V.KChar 'c') [V.MCtrl] -> BM.halt st
+    V.EvKey (V.KChar 'q') []        -> BM.halt st
+    _                               -> BM.continue st
 appEvent st _ = BM.continue st
 
 drawUI :: () -> [BT.Widget ()]
-drawUI st = []
+drawUI st = [BW.str "Test"]
 
 theApp :: BM.App () e ()
 theApp = BM.App
