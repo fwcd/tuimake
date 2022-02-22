f6a6693176e84a62c21b1b3fc06fd4137a2abbc8
fwcd <fwcdmail@gmail.com>
2022-02-22 15:12:25 +0100

    Parse basic --help argument

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,15 @@
 module Main where
 
+import System.Environment
 import Lib
 
 main :: IO ()
-main = someFunc
+main = do
+  args <- getArgs
+  
+  case args of
+    ["--help"] -> putStrLn usage
+    ["-h"]     -> putStrLn usage
+    _          -> putStrLn "TODO"
+  
+  where usage = "Usage: tuimake [target...]"
